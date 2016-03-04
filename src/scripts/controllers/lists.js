14f6277c54aab7e3ebd46ff21e347b4dcb46941e
Lorin Steel <lorinsteel@gmail.com>
2016-03-03 17:31:29 -0700

    updated list and login partials, list controller, and added data structure example

--- src/scripts/controllers/lists.js
+++ src/scripts/controllers/lists.js
@@ -16,22 +16,5 @@ angular
     listsCtrl.signout = function(email, password) {
       return authFuncs.logout();
     };
-
-    // listsCtrl.inputType = 'signin';
-    //
-    // listsCtrl.submit = function(email, password) {
-    //   listsCtrl.errorMessage = null;
-    //
-    //   listsCtrl[listsCtrl.inputType](email, password);
-    // };
-    //
-    // listsCtrl.signin = function(email, password) {
-    //   return authFuncs.login(email, password);
-    // };
-    //
-    // listsCtrl.signup = function(email, password) {
-    //   return authFuncs.create(email, password);
-    // };
-    // return listsCtrl;
   },
 ]);
