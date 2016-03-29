6025c548c719ee4f37912c8f79db7c6a1a0e8e0a
lsteel <lorinsteel@gmail.com>
2016-03-28 20:04:36 -0600

    Larger gravatar for retina screens

--- src/scripts/controllers/lists.js
+++ src/scripts/controllers/lists.js
@@ -88,7 +88,7 @@ angular
       userSettings.get(listsCtrl.uid, function(settingsObj) {
         listsCtrl.settings = settingsObj;
         //http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200
-        listsCtrl.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash(listsCtrl.settings.email.trim() || '') + '?s=80.jpg';
+        listsCtrl.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash(listsCtrl.settings.email.trim() || '') + '.jpg';
         console.log(listsCtrl.settings);
       });
 
