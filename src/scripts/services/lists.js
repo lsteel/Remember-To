759fd86d655b17a637007d0e83e8d81b142abd65
lsteel <lorinsteel@gmail.com>
2016-04-07 18:51:42 -0600

    fixed continuous loading issue for new accounts with no lists.

--- src/scripts/services/lists.js
+++ src/scripts/services/lists.js
@@ -1,174 +1,180 @@
 angular
-  .module('appLists', [
-    'appUsers',
-    'appTasks',
-  ])
-  .factory('lists', [
-    'users',
-    'tasks',
-    '$firebaseObject',
-    '$firebaseArray',
-    function(users, tasks, $firebaseObject, $firebaseArray) {
+.module('appLists', [
+  'appUsers',
+  'appTasks',
+])
+.factory('lists', [
+  'users',
+  'tasks',
+  '$firebaseObject',
+  '$firebaseArray',
+  function(users, tasks, $firebaseObject, $firebaseArray) {
 
-      var lists = {
+    var lists = {
 
-        watchList: function(lid, cb) {
-          var watchListsRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
-          var watchListsArr = $firebaseArray(watchListsRef);
-          watchListsArr.$watch(function() {
-              cb(true);
+      watchList: function(lid, cb) {
+        var watchListsRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
+        var watchListsArr = $firebaseArray(watchListsRef);
+        watchListsArr.$watch(function() {
+          cb(true);
+        });
+      },
+
+      watchSettings: function(uid, cb) {
+        var watchListsRef = new Firebase("https://remto.firebaseio.com/users/" + uid + '/lists/');
+        var watchListsArr = $firebaseArray(watchListsRef);
+        watchListsArr.$watch(function() {
+          cb(true);
+        });
+      },
+
+      create: function(userID, inputs, cb) {
+        var listsRef = new Firebase("https://remto.firebaseio.com/lists/");
+        var listPushRef = listsRef.push();
+        var fireLists = $firebaseObject(listPushRef);
+
+        var listKey = fireLists.$id;
+        fireLists.listName = inputs.name.trim().toLowerCase();
+        fireLists.users = [inputs.users];
+        fireLists.owners = [inputs.users];
+
+        fireLists.$save().then(function() {
+          return cb(listKey, inputs);
+        }, function(error) {
+          console.log("Error:", error);
+        });
+      },
+
+      update: function(lid, inputs, cb) {
+        var listRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
+        var fireList = $firebaseObject(listRef);
+        fireList.$loaded().then(function() {
+          fireList.listName = inputs.name.trim().toLowerCase();
+          fireList.users = inputs.users;
+          fireList.owners = inputs.users;
+          cb();
+          fireList.$save().then(function() {
           });
-        },
+        });
 
-        watchSettings: function(uid, cb) {
-          var watchListsRef = new Firebase("https://remto.firebaseio.com/users/" + uid + '/lists/');
-          var watchListsArr = $firebaseArray(watchListsRef);
-          watchListsArr.$watch(function() {
-              cb(true);
-          });
-        },
+        // var fireList = $firebaseObject(listRef);
+        //
+        // fireList.listName = inputs.name;
+        // fireList.users = inputs.users;
+        // fireList.owners = inputs.users;
+        // fireList.tasks = fireList.tasks || null;
+        //
+        // fireList.$save().then(function() {
+        //   //console.log('List set.');
+        //   return cb();
+        // }, function(error) {
+        //   console.log("Error:", error);
+        // });
+      },
 
-        create: function(userID, inputs, cb) {
-          var listsRef = new Firebase("https://remto.firebaseio.com/lists/");
-          var listPushRef = listsRef.push();
-          var fireLists = $firebaseObject(listPushRef);
-
-          var listKey = fireLists.$id;
-          fireLists.listName = inputs.name.trim().toLowerCase();
-          fireLists.users = [inputs.users];
-          fireLists.owners = [inputs.users];
-
-          fireLists.$save().then(function() {
-            return cb(listKey, inputs);
-          }, function(error) {
-            console.log("Error:", error);
-          });
-        },
-
-        update: function(lid, inputs, cb) {
+      delete: function(userID, lid, lsid, cb) {
+        lists.deleteSettings(userID, lid, lsid, function(lid) {
           var listRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
           var fireList = $firebaseObject(listRef);
-          fireList.$loaded().then(function() {
-            fireList.listName = inputs.name.trim().toLowerCase();
-            fireList.users = inputs.users;
-            fireList.owners = inputs.users;
+          fireList.$remove().then(function() {
             cb();
-            fireList.$save().then(function() {
-            });
           });
+        });
+      },
 
-          // var fireList = $firebaseObject(listRef);
-          //
-          // fireList.listName = inputs.name;
-          // fireList.users = inputs.users;
-          // fireList.owners = inputs.users;
-          // fireList.tasks = fireList.tasks || null;
-          //
-          // fireList.$save().then(function() {
-          //   //console.log('List set.');
-          //   return cb();
-          // }, function(error) {
-          //   console.log("Error:", error);
-          // });
-        },
+      deleteSettings: function(userID, lid, lsid, cb) {
+        var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/" + lsid);
+        var fireUserListsSettings = $firebaseObject(userListsSettingsRef);
 
-        delete: function(userID, lid, lsid, cb) {
-          lists.deleteSettings(userID, lid, lsid, function(lid) {
-            var listRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
-            var fireList = $firebaseObject(listRef);
-            fireList.$remove().then(function() {
-              cb();
-            });
-          });
-        },
+        fireUserListsSettings.$remove().then(function() {
+          cb(lid);
+        });
+      },
 
-        deleteSettings: function(userID, lid, lsid, cb) {
-          var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/" + lsid);
-          var fireUserListsSettings = $firebaseObject(userListsSettingsRef);
-
-          fireUserListsSettings.$remove().then(function() {
-            cb(lid);
-          });
-        },
-
-        getSingle: function(userID, lid, cb) {
-          lists.getSingleSettings(userID, lid, function(listSettings, lid) {
-            var list = {};
-            var listRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
-            var fireList = $firebaseObject(listRef);
-            fireList.$loaded().then(function(fireList) {
-              if (fireList.$value === null) {
-                cb(null);
-              }
-              else {
-                list = fireList;
-                list.doNotDisturb = listSettings[0].doNotDisturb;
-                list.location = listSettings[0].location;
-                list.color = listSettings[0].color;
-                list.icon = listSettings[0].icon;
-                list.lid = listSettings[0].lid;
-                list.sortOrder = listSettings[0].sortOrder;
-                list.userSettingsID = listSettings[0].$id;
-                cb(list);
-              }
-            });
-          });
-        },
-
-        getSingleSettings: function(userID, lid, cb) {
-          var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/");
-          var fireUserListsSettings = $firebaseArray(userListsSettingsRef);
-
-          fireUserListsSettings.$loaded().then(function() {
-            function isList(obj) {
-              return obj.lid == lid;
+      getSingle: function(userID, lid, cb) {
+        lists.getSingleSettings(userID, lid, function(listSettings, lid) {
+          var list = {};
+          var listRef = new Firebase("https://remto.firebaseio.com/lists/" + lid);
+          var fireList = $firebaseObject(listRef);
+          fireList.$loaded().then(function(fireList) {
+            if (fireList.$value === null) {
+              cb(null);
             }
-            var singleListSettings = fireUserListsSettings.filter(isList);
-            cb(singleListSettings, lid);
+            else {
+              list = fireList;
+              list.doNotDisturb = listSettings[0].doNotDisturb;
+              list.location = listSettings[0].location;
+              list.color = listSettings[0].color;
+              list.icon = listSettings[0].icon;
+              list.lid = listSettings[0].lid;
+              list.sortOrder = listSettings[0].sortOrder;
+              list.userSettingsID = listSettings[0].$id;
+              cb(list);
+            }
           });
-        },
+        });
+      },
 
-        getAll: function(userID, cb) {
-          lists.getUserListsSettings(userID, function(userListsSettings) {
-            lists.getUserLists(userID, userListsSettings, function(userLists) {
-              cb(userLists);
-            });
+      getSingleSettings: function(userID, lid, cb) {
+        var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/");
+        var fireUserListsSettings = $firebaseArray(userListsSettingsRef);
+
+        fireUserListsSettings.$loaded().then(function() {
+          function isList(obj) {
+            return obj.lid == lid;
+          }
+          var singleListSettings = fireUserListsSettings.filter(isList);
+          cb(singleListSettings, lid);
+        });
+      },
+
+      getAll: function(userID, cb) {
+        lists.getUserListsSettings(userID, function(userListsSettings) {
+          lists.getUserLists(userID, userListsSettings, function(userLists) {
+            cb(userLists);
           });
-        },
+        });
+      },
 
-        getUserListsSettings: function(userID, cb) {
-          var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/");
-          var fireUserListsSettings = $firebaseArray(userListsSettingsRef);
+      getUserListsSettings: function(userID, cb) {
+        var userListsSettingsRef = new Firebase("https://remto.firebaseio.com/users/" + userID + "/lists/");
+        var fireUserListsSettings = $firebaseArray(userListsSettingsRef);
 
-          fireUserListsSettings.$loaded().then(function() {
-            cb(fireUserListsSettings);
-          });
-        },
+        fireUserListsSettings.$loaded().then(function() {
+          cb(fireUserListsSettings);
+        });
+      },
 
-        getUserLists: function(userID, userListsSettings, cb) {
-          var i = 0;
-          var userLists = [];
-          userListsSettings.forEach(function(item, index, array) {
-            var listRef = new Firebase("https://remto.firebaseio.com/lists/" + item.lid);
-            var fireList = $firebaseObject(listRef);
-            fireList.$loaded().then(function(fireList) {
-              userLists[index] = fireList;
-              userLists[index].color = userListsSettings[index].color;
-              userLists[index].icon = userListsSettings[index].icon;
-              userLists[index].lid = userListsSettings[index].lid;
-              userLists[index].doNotDisturb = userListsSettings[index].doNotDisturb;
-              userLists[index].locations = userListsSettings[index].locations || null;
-              userLists[index].userSettingsID = userListsSettings[index].$id;
-              userLists[index].sortOrder = userListsSettings[index].sortOrder;
-              i++;
-              if (i === array.length) {
-                cb(userLists);
-              }
-            });
-          });
+      getUserLists: function(userID, userListsSettings, cb) {
+        var i = 0;
+        var userLists = [];
+        if (userListsSettings.length === 0) {
+          cb(userLists);
+          return;
         }
-      };
+        userListsSettings.forEach(function(item, index, array) {
+          var listRef = new Firebase("https://remto.firebaseio.com/lists/" + item.lid);
+          var fireList = $firebaseObject(listRef);
+          fireList.$loaded().then(function(fireList) {
+            userLists[index] = fireList;
+            userLists[index].color = userListsSettings[index].color;
+            userLists[index].icon = userListsSettings[index].icon;
+            userLists[index].lid = userListsSettings[index].lid;
+            userLists[index].doNotDisturb = userListsSettings[index].doNotDisturb;
+            userLists[index].locations = userListsSettings[index].locations || null;
+            userLists[index].userSettingsID = userListsSettings[index].$id;
+            userLists[index].sortOrder = userListsSettings[index].sortOrder;
+            i++;
+            if (i === array.length) {
+              cb(userLists);
+            }
+          }).catch(function(error) {
+            console.error("Error:", error);
+          });
+        });
+      }
+    };
 
-      return lists;
-    },
-  ]);
+    return lists;
+  },
+]);
